217ffc8d968f8f0a3ada5e1683700bf62cc06876
codepoet80 <curator@webosarchive.com>
2021-02-27 19:11:30 +0000

    server changes

--- add-reddit.php
+++ add-reddit.php
@@ -26,33 +26,42 @@ if ($client_key != '' && $debug_key != '') {	//If configuration includes both cl
             echo "{\"status\": \"error\", \"msg\": \"ERROR: No authorized user.\"}";
             die;
         }
-	}
+    }
 }
 
 if ($server_id == '' || ($server_id != '' && strpos($request, $server_id) !== false))		//If configuration includes a server key value, enforce it
 {
-	//decode inbound request
-	$request = str_replace($server_id, "", $request);
-	$request = base64_decode($request); //Requested Reddit URL
+    //decode inbound request
+    $request = str_replace($server_id, "", $request);
+    $request = base64_decode($request); //Requested Reddit URL
     $request = extract_reddit_video_link($request);    //Converted Reddit video URL
 
-    $try_ffmpeg = trim(shell_exec('type -P ffmpeg'));
-    if (empty($try_ffmpeg)) {
+//echo "request: " . $request;
+
+    $try_ffmpeg = trim(shell_exec('type ffmpeg'));
+
+//echo "\r\nffmpeg: " . $try_ffmpeg;
+
+    if (empty($try_ffmpeg) || strpos($try_ffmpeg, "not found") !== false) {
         echo "{\"status\": \"error\", \"msg\": \"ERROR: FFMpeg not found on server.\"}";
         die;
     }
+echo "about to do command";
 
     $preset = 'fast';
     $crf = 20;
-    $command = "ffmpeg -i $request -c:v libx264 -preset $preset -crf $crf $save_as.mp4";
+    $command = "ffmpeg -i $request -c:v libx264 -preset $preset -crf $crf 'save_as.mp4'";
+    echo $command;
+    die;
     echo shell_exec($command);
 
-	if ($err) {
-	  echo "Request error:" . $err;
-	}
-	else {
-	  	echo $response;
-	}
+    if ($err) {
+	echo "{\"status\": \"error\", \"msg\": \"ERROR: Download request error.\"}";
+        die;
+    }
+    else {
+        echo $response;
+    }
 }
 else
 {
@@ -62,11 +71,13 @@ else
 function extract_reddit_video_link(string $post_url)
 {
     if (!isset($post_url) or trim($post_url) == '' or strpos($post_url, 'reddit.com') === false) {
-        throw new Exception("You must provide a Reddit post url");
+	echo "{\"status\": \"error\", \"msg\": \"ERROR: No Reddit URL found in request.\"}";
+	die;
     }
     $data = json_decode(file_get_contents("" . $post_url . ".json"), true);
     if ($data[0]['data']['children'][0]['data']['secure_media']['reddit_video']['is_gif'] == true) {
-        throw new Exception("Video is actually a gif");
+	echo "{\"status\": \"error\", \"msg\": \"ERROR: Requested video is actuall a gif\"}";
+	die;
     }
     $video_link = $data[0]['data']['children'][0]['data']['secure_media']['reddit_video']['dash_url'];
     return $video_link;
