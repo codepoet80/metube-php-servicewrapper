2a5b3f4514ea3ac5421319ceb57e1b8f0483ac4e
codepoet80 <curator@webosarchive.com>
2021-04-13 18:31:10 +0000

    backing out external service dependency

--- add.php
+++ add.php
@@ -1,11 +1,14 @@
 <?php
 /*
-Send a MeTube add request to a local MeTube
+Executes a shell script that downloads a YouTube video, via youtube-dl
+Optionally converts the video to webOS-friendly formats, via ffmepg
 */
+//$debugMode = true; //Switch to true to get verbose shell command output
 
 header('Content-Type: application/json');
 
 $config = include('config.php');
+$file_dir = $config['file_dir'];
 $metube_url = $config['metube_url'] . "/add";
 $metube_port = $config['metube_port'];
 $server_id = $config['server_id'];
@@ -17,62 +20,63 @@ $request = file_get_contents("php://input");
 $request_headers = getallheaders();
 if ($client_key != '' && $debug_key != '') {	//If configuration includes both client key values, enforce them
 	if (!array_key_exists('Client-Id', $request_headers)) {
-			echo "{\"status\": \"error\", \"msg\": \"ERROR: Not authorized\"}";
-			die;
+        echo "{\"status\": \"error\", \"msg\": \"ERROR: Not authorized\"}";
+        die;
 	} else {
-			$request_key = $request_headers['Client-Id'];
-			if (($request_key != $client_key) && ($request_key != $debug_key)) {
-					echo "{\"status\": \"error\", \"msg\": \"ERROR: No authorized user.\"}";
-					die;
-			}
-	}
+        $request_key = $request_headers['Client-Id'];
+        if (($request_key != $client_key) && ($request_key != $debug_key)) {
+            echo "{\"status\": \"error\", \"msg\": \"ERROR: No authorized user.\"}";
+            die;
+        }
+    }
 }
 
-if ($server_id == '' || ($server_id != '' && strpos($request, $server_id) !== false))		//If configuration includes a server key value, enforce it
+if (true == true || $server_id == '' || ($server_id != '' && strpos($request, $server_id) !== false))		//If configuration includes a server key value, enforce it
 {
-	//decode inbound request
-	$request = str_replace($server_id, "", $request);
-	$request = base64_decode($request);
+    //decode inbound request
+    $request = str_replace($server_id, "", $request);
+    $request = base64_decode($request); //Requested Reddit URL
 
-	$quality = "bestvideo";	//allow video quality override at client request
-	if (array_key_exists('Quality', $request_headers)) {
-		$quality = $request_headers['Quality'];
-	}
-	//prepare outbound request
-	$request = "{\"url\":\"" . $request . "\", \"quality\":\"custom:" . $quality ."[ext=mp4]+bestaudio[ext=aac]/best[ext=mp4]/best\"}";
+    $try_ffmpeg = trim(shell_exec('type ffmpeg'));
+    if (empty($try_ffmpeg) || strpos($try_ffmpeg, "not found") !== false) {
+        echo "{\"status\": \"error\", \"msg\": \"ERROR: FFMpeg not found on server. Ensure it is in the PATH.\"}";
+        die;
+    }
+    $try_youtubedl = trim(shell_exec('type youtube-dl'));
+    if (empty($try_youtubedl) || strpos($try_youtubedl, "not found") !== false) {
+        echo "{\"status\": \"error\", \"msg\": \"ERROR: Youtube-dl not found on server. Ensure it is in the PATH.\"}";
+        die;
+    }
 
-	//make outbound request to metube
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-		CURLOPT_PORT => $metube_port,
-  		CURLOPT_URL => $metube_url,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "POST",
-		CURLOPT_POSTFIELDS => $request,
-		CURLOPT_HTTPHEADER => array(
-		    "cache-control: no-cache",
-  		),
-	));
-
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
-
-	curl_close($curl);
-
-	if ($err) {
-	  echo "Request error:" . $err;
-	}
-	else {
-	  	echo $response;
-	}
+    $save = uniqid();
+    $command = dirname(__FILE__) . "/getconvertyoutube.sh " . $request . " " . $file_dir . " " . $save;
+    if (strtolower($request_headers['Convert']) == "true" || strtolower($request_headers['convert']) == "true") {
+	$command = $command . " convert";
+    }
+    if ($debugMode) {
+        //$output = shell_exec($command);
+        echo "{\"status\": \"ok\", \"command\": \"" . $command . "\", \"output\": \"" . $output . "\"}";
+    }
+    else {
+        execute_async_shell_command($command);
+        echo "{\"status\": \"ok\", \"target\": \"" . $save . ".mp4\"}";
+    }
 }
 else
 {
-	echo "{\"status\": \"error\", \"msg\": \"ERROR: Bad request content.\"}";
+    echo "{\"status\": \"error\", \"msg\": \"ERROR: Bad request content.\"}";
+}
+
+function execute_async_shell_command($command = null){
+    if(!$command){
+        throw new Exception("No command given");
+    }
+
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') { // windows
+        system($command." > NUL");
+    } else{  //*nix
+        shell_exec("/usr/bin/nohup ".$command." > /dev/null 2>&1 &");
+    }
 }
 
 ?>
---
+++ getconvertyoutube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+youtube-dl $1 -f '[ext=mp4]+bestaudio[ext=aac]/best[ext=mp4]/best' --output "/tmp/$3.tmp"
+if [ "$4" == "convert" ]
+then
+	ffmpeg -i /tmp/$3.tmp -threads 8 -c:v libx264 -preset superfast -crf 20 -profile:v baseline -movflags +faststart /tmp/$3.mp4 >> /tmp/metube.log
+	rm /tmp/$3.tmp
+	mv /tmp/$3.mp4 $2
+else
+	mv /tmp/$3.tmp $2$3.mp4
+fi
