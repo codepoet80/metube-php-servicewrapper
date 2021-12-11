a2dbfc140ec8c8780c7d6c6ad0ca46297e2fb8a2
CodePoet80 <jonathan.m.wise@gmail.com>
2021-12-11 09:40:06 -0500

    ug

--- play.php
+++ play.php
@@ -90,6 +90,7 @@ if (file_exists($file_name)) {
 		$file_name_parts = explode("/", $file_name);
 		$file_name = end($file_name_parts);
 
+		echo ($dir);
 		$dirparts = explode("/", $dir);
 		$dir = end($dirparts);
         die("Location: /" . $dir . "/" .$file_name);
