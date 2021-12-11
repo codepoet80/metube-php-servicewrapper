7a88bf09358b838be7d91966e4e74b96ad451400
CodePoet80 <jonathan.m.wise@gmail.com>
2021-12-11 09:25:46 -0500

    update config sample

--- config-sample.php
+++ config-sample.php
@@ -3,6 +3,7 @@ return array(
 	'api_key' => 'YOURAPIKEY',	//Your Google API Key to enable YouTube search features
 	'file_dir' => '/FOLDER/TO/SAVE/VIDEOS/TO',
 	'obscure_filepath' => false, //If set to true, the actual URL of the video will be hidden by PHP. If set to false, you must expose the download directory.
+	'use_xsendfile' => false, //Not used if obscure_filepath is false. Make sure you have your Apache and htaccess configured before turning this on
 	'client_key' => '',	//If you want some security, put a random string here and in debug_key. Your client must know this value.
 	'debug_key' => '',	//If you want some security, put a random string here and in client_key. Your client must know this value.
 	'server_id' => ''	//If you want some more security, put a random string here. Your client must know this value.
